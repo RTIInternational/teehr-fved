bfbb37f6fee92aee441f5334d2e3720a52e6fa5c
Matt Denno <mgdenno@gmail.com>
2026-08-24 08:36:59 -0400

    update deployment for polaris support

--- warehouse/local/setup_utils.py
+++ warehouse/local/setup_utils.py
@@ -1,5 +1,3 @@
-from teehr.evaluation.spark_session_utils import create_spark_session
-
 # primary location ids for FVED
 DEV_LOCATION_ID_LIST = [
     # initial crmms locations w/ usgs record
@@ -7,17 +5,3 @@ DEV_LOCATION_ID_LIST = [
     'usgs-09361500',
     'usgs-09260050'
 ]
-
-
-
-def create_minio_spark_session():
-    """Start a Spark session with MinIO credentials and custom configuration."""
-    
-    return create_spark_session(
-    aws_access_key_id="minioadmin",
-    aws_secret_access_key="minioadmin123",
-    update_configs={
-        "spark.hadoop.fs.s3a.aws.credentials.provider":
-        "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider"
-    }
-)
