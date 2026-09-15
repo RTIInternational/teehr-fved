186c1574550f9e2bc971868cd899d9eeff34e989
samlamont <sam.lamont@gmail.com>
2026-09-15 11:41:38 -0400

    chore: add usgs basins for local dev

--- warehouse/local/setup_utils.py
+++ warehouse/local/setup_utils.py
@@ -3,5 +3,32 @@ DEV_LOCATION_ID_LIST = [
     # initial crmms locations w/ usgs record
     'usgs-09149500',
     'usgs-09361500',
-    'usgs-09260050'
+    'usgs-09260050',
+    # sample usgs basin polygon ids
+    'usgsbasin-06114700',
+    'usgsbasin-06123030',
+    'usgsbasin-06123500',
+    'usgsbasin-06126050',
+    'usgsbasin-06126500',
+    'usgsbasin-06127500',
+    'usgsbasin-06130500',
+    'usgsbasin-06131000',
+    'usgsbasin-06131200',
+    'usgsbasin-06177500',
+    'usgsbasin-06204500',
+    'usgsbasin-06205000',
+    'usgsbasin-06206500',
+    'usgsbasin-06207500',
+    'usgsbasin-06208500',
+    'usgsbasin-06209500',
+    'usgsbasin-06211000',
+    'usgsbasin-06211500',
+    'usgsbasin-06212500',
+    'usgsbasin-06214500',
+    'usgsbasin-06216000',
+    'usgsbasin-06216900',
+    'usgsbasin-06220500',
+    'usgsbasin-06221400',
+    'usgsbasin-06222700',
+    'usgsbasin-06224000'
 ]
